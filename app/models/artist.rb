d46005470176ca62a0531b4f784f5cf322b28668
Carla Holcomb <carla.programmer@gmail.com>
2020-01-15 00:37:50 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -13,11 +13,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    count.songs
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    count.genres
+    self.genres.count
   end
 end
